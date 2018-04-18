fd7448a4901479a6225bdc62185203442f839ed0
Muhammadrizki1998 <38484812+Muhammadrizki1998@users.noreply.github.com>
2018-04-18 04:27:50 +0000

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,18 @@
+import M.Rizky.ifelse
+import M.Rizky.random
+import M.Rizky.append
+import M.Rizky.tupple
+import M.Rizky.dict
+import M.Rizky.opp
+
+print (M.Rizky.ifelse.fa())
+
+print (M.Rizky.random.fb())
+
+print (M.Rizky.append.fc())
+
+print (M.Rizky.tupple.fd())
+
+print (M.Rizky.dict.fe())
+
+print (M.Rizky.opp.ff())
